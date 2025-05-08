d1eaa0fc0d43ec6d691022ca3a105454d37a5b91
Saveli Gulas <saveli.gulas@gmail.com>
2025-05-09 00:57:28 +0200

    small update

--- akka/src/main/java/fhv/aktor/akka/GuardianActor.java
+++ akka/src/main/java/fhv/aktor/akka/GuardianActor.java
@@ -2,7 +2,10 @@ package fhv.aktor.akka;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
-import akka.actor.typed.javadsl.*;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
 import fhv.aktor.akka.command.blackboard.BlackboardCommand;
 import fhv.aktor.akka.subordinate.sensor.TemperatureSensor;
 
